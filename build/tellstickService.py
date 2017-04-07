0895e38ca55edca789fafabadffa0916ad6b6f03
Jimmy Fjällid <jimmy@fjallid.com>
2017-04-07 12:39:20 +0200

    added useful log messages

--- build/tellstickService.py
+++ build/tellstickService.py
@@ -23,11 +23,15 @@ baudrate=9600
 mydevice = None
 tellstick = TellStick(mode='t')
 tellstick.flush()
-t1 = None
-t2 = None
 target = []
 shared_dict = {}
 lock = threading.Lock()
+
+try:
+  configpath = sys.argv[1]
+except:
+  print "Usage: tellstickService.py <path to config directory>"
+  sys.exit(1)
 
 def reader():
     while True:
@@ -64,11 +68,6 @@ def writer():
         else:
             print "nothing to send"
             time.sleep(0.1)
-def join():
-    # Use of a timeout allows Ctrl-C interruption
-    t1.join(timeout=1e6)
-    t2.join(timeout=1e6)
-
 
 def parseReading(msg):
     mswitch = re.search(r"protocol:([a-z]+);model:([a-z]+);data:(.+);", msg)
@@ -89,14 +88,15 @@ def parseReading(msg):
           model = "selflearning-switch"  
         for s in listeners:
           if str(s.protocol) == protocol and s.model == model and s.house == house and s.unit == unit:
-            print "updated timestamp for " + str(s.id)
+            print "Telldus device " + str(s.id) + " changed to state " + str(method)
             with lock:
               shared_dict[s.id] = (datetime.datetime.now(), method)
             deviceType = str(s.__class__.__name__)
             topic = deviceType + "s/tellstick/"+ s.mqttRoom +"/"+ s.mqttDescription
             my_publish(topic, method)
-#          else:
-#            print "Switch was not found in config"
+            return
+          
+        print "Not found in config [protocol:" + str(protocol) + ";model:" + str(model) + ";house:" + str(house) + ";unit:" + str(unit) + ";method:" + str(method) + ";]"
             
     elif msensor is not None:
       protocol = msensor.group(1)
@@ -115,11 +115,17 @@ def parseReading(msg):
           if str(s.protocol) == protocol and s.model == model and s.id == sensorID:
             my_publish("sensors/" + s.mqttRoom + "/temperature/" + str(s.id) + "/sensors", temp)
             if model == "temperaturehumidity":
-              my_publish("sensors/" + s.mqttRoom + "/humidity/" + str(s.id) + "/sensors", humidity) 
+              my_publish("sensors/" + s.mqttRoom + "/humidity/" + str(s.id) + "/sensors", humidity)
+            return
+        unknownSensor = "Not found in config[class:sensor;protocol:" + str(protocol) + ";id:" + str(id) + ";model:" + str(model)
+        if model == "temperature":
+          unknownSensor += ";temp:" + str(temp) + ";]"
+        elif model == "temperaturehumidity":
+          unknownSensor += ";humidity:" + str(humidity) + ";temp:" + str(temp) + ";]" 
               
 
     
-with open("./config.yaml", 'r') as stream:
+with open(configpath + "/config.yaml", 'r') as stream:
   out = yaml.safe_load(stream)
   print "[info] getting the mqtt settings"
   mqtt_host = out['mqtt']['host']
@@ -139,12 +145,10 @@ with open("./config.yaml", 'r') as stream:
 
   print "[info] getting my switches"
   listeners = []
-  id = 0
   try:
     device = namedtuple("device", "mqttRoom mqttDescription protocol model house unit id")
     for sw in out['switch']:
-      myDevice = device(sw['mqtt']['room'],sw['mqtt']['description'],sw['device']['protocol'],sw['device']['model'],sw['device']['house'],sw['device']['unit'], id)
-      id += 1
+      myDevice = device(sw['mqtt']['room'],sw['mqtt']['description'],sw['device']['protocol'],sw['device']['model'],sw['device']['house'],sw['device']['unit'], str(sw['mqtt']['room'] + '/' + sw['mqtt']['description']))
       listeners.append(myDevice)
   except:
     print "[info] No switches in config"
@@ -153,8 +157,7 @@ with open("./config.yaml", 'r') as stream:
   try:
     trigger = namedtuple("trigger", "mqttRoom mqttDescription protocol model house unit id")
     for r in out['trigger']:
-      myTrigger = trigger(r['mqtt']['room'],r['mqtt']['description'],r['device']['protocol'],r['device']['model'],r['device']['house'],r['device']['unit'], id)
-      id += 1
+      myTrigger = trigger(r['mqtt']['room'],r['mqtt']['description'],r['device']['protocol'],r['device']['model'],r['device']['house'],r['device']['unit'], str(sw['mqtt']['room'] + '/' + sw['mqtt']['description']))
       listeners.append(myTrigger)
   except:
     print "[info] No switches in config"
@@ -169,26 +172,10 @@ def action_sub_thread():
     client.connect(mqtt_host, mqtt_port, 60)
     client.loop_forever()
 
-
-def listen_thread():
-    print "start tellstick listen thread"
-    try:
-        if loop:
-            loop.run_forever()
-        else:
-            import time
-            while True:
-                core.callback_dispatcher.process_pending_callbacks()
-                time.sleep(0.5)
-    except KeyboardInterrupt:
-        pass
-
-
 def on_connect(client, userdata, rc):
     client.subscribe("devices/tellstick/+/+")
 
 def mqtt_trigger_handler(room,description,msg):
-#    print "I'm in trigger handler"
     for sw in listeners:
       if sw.mqttRoom == room  and  sw.mqttDescription == description:
         now = datetime.datetime.now()
@@ -201,23 +188,18 @@ def mqtt_trigger_handler(room,description,msg):
           elapsed = datetime.timedelta(seconds=10)
           lastState = 0
           
-        if elapsed > datetime.timedelta(seconds=5) :
+        if elapsed > datetime.timedelta(seconds=5) or int(lastState) != int(msg.payload):
+          print "MQTT device " + str(sw.id) + " setting state " + str(msg.payload)
           q.put( (sw.protocol, sw.model, sw.house, sw.unit, msg.payload) )
           with lock:
             shared_dict[sw.id] = (datetime.datetime.now(), msg.payload)
-          print "message put in queue for id " + str(sw.id)
-        else:
-          print "Will not repeat sniffed message"
         return True
     return False
 
 
 def on_message(client, userdata, msg):
     m = re.search(r"devices\/tellstick\/([a-zA-Z0-9_]{1,30})\/([a-zA-Z0-9_]{1,30})",msg.topic)
-    # print "received message:"
-    # print m.group(1)
-    # print m.group(2)
-    # print msg.payload
+    
     if m:
         if mqtt_trigger_handler(m.group(1),m.group(2),msg) == False:
           print "Not listed trigger in room: [" + m.group(1) + "] with  description: [" + m.group(2)+ "]"
@@ -228,14 +210,6 @@ def my_publish(topic, message):
     publish.single(topic, payload=message, qos=0, retain=False, hostname=mqtt_host,
     port=mqtt_port, client_id="", keepalive=60, will=None, auth= {'username':mqtt_username, 'password':mqtt_password}, tls=None,
     protocol=mqtt.MQTTv31)
-
-def find_device(device, devices):
-    for d in devices:
-        print d
-        if str(d.id) == device or d.name == device:
-            return d
-    print("Device '{}' not found".format(device))
-    return None
 
 print "start thread 1"
 t = threading.Thread(target=action_sub_thread, args = ())
