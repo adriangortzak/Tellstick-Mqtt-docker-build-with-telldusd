b238cdcafe82b2f2eb647dea40791579b5e56309
Jimmy Fjällid <jimmy@fjallid.com>
2017-04-16 20:18:44 +0200

    Added support for logging to syslog and added support for dimmer devices

--- build/tellstickService.py
+++ build/tellstickService.py
@@ -1,4 +1,3 @@
-
 import argparse
 import sys
 import paho.mqtt.client as mqtt
@@ -13,6 +12,8 @@ import datetime
 from pylibftdi import USB_PID_LIST, USB_VID_LIST, Device as TellStick
 from rf433.Protocol import Protocol, Device
 from Queue import *
+import logging
+import logging.handlers
 
 USB_PID_LIST.append(0x0c31)
 USB_VID_LIST.append(0x1781)
@@ -27,11 +28,34 @@ target = []
 shared_dict = {}
 lock = threading.Lock()
 
+my_logger = logging.getLogger('MyLogger')
+#my_logger.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('tellstickService.py [%(levelname)s] %(message)s')
+
+handler = logging.handlers.SysLogHandler(address = '/dev/log')
+handler.setFormatter(formatter)
+#handler.setLevel(logging.DEBUG)
+
+#my_logger.addHandler(handler)
+
 try:
   configpath = sys.argv[1]
 except:
   print "Usage: tellstickService.py <path to config directory>"
   sys.exit(1)
+
+def myPrint(message, level):
+    if str(level) == "INFO":
+      print message
+      my_logger.info(message)
+    elif str(level) == "ERROR":
+      print message
+      my_logger.error(message)
+    elif str(level) == "DEBUG":
+      print message
+      my_logger.debug(message)
 
 def reader():
     while True:
@@ -51,24 +75,33 @@ def writer():
         unit = item[3]
         command = item[4]
         sendcount = item[5]
+        type = item[6]
+        myPrint("Received work order: " + str(protocol) + " " + str(model) + " " + str(house) + " " + str(unit) + " " + str(command) + " " + str(sendcount), "DEBUG")
         msg = ""
         
         myDevice = Protocol.protocolInstance(protocol)
         myDevice.setModel(model)
         myDevice.setParameters({'house': house, 'unit': unit})
         
-        if command == "0":
-          msg = myDevice.stringForMethod(Device.TURNOFF, 0)
-        elif command == "1":
-          msg = myDevice.stringForMethod(Device.TURNON, 0)
+        if str(type) == "onoff":
+          if command == "0":
+            myPrint("Sending command TURNON", "INFO")
+            msg = myDevice.stringForSelflearning(Device.TURNOFF, 0)
+          elif command == "1":
+            myPrint("Sending command TURNOFF", "INFO")
+            msg = myDevice.stringForMethod(Device.TURNON, 0)
+        elif str(type) == "dimmer":
+          myPrint("Sending command DIM " + str(command), "INFO")
+          msg = myDevice.stringForMethod(Device.DIM, int(command))
+  
         if 'S' in msg:
           toSend = 'S%s+' % msg['S']
           for x in range(0,sendcount):
             tellstick.write(toSend)
             time.sleep(1) # Make sure you sleep or else it won't like getting another command so soon! Was 2 seconds before
         else:
-            print "nothing to send"
-            time.sleep(0.1)
+          myPrint("Invalid stringForMethod response", "DEBUG")
+          time.sleep(0.1)
 
 def parseReading(msg):
     mswitch = re.search(r"protocol:([a-z]+);model:([a-z]+);data:(.+);", msg)
@@ -89,7 +122,7 @@ def parseReading(msg):
           model = "selflearning-switch"  
         for s in listeners:
           if str(s.protocol) == protocol and s.model == model and s.house == house and s.unit == unit:
-            print "Telldus device " + str(s.id) + " changed to state " + str(method)
+            myPrint("Sniffed that Telldus device " + str(s.id) + " changed to state " + str(method), "INFO")
             with lock:
               shared_dict[s.id] = (datetime.datetime.now(), method)
             deviceType = str(s.__class__.__name__)
@@ -97,7 +130,7 @@ def parseReading(msg):
             my_publish(topic, method)
             return
           
-        print "Not found in config [protocol:" + str(protocol) + ";model:" + str(model) + ";house:" + str(house) + ";unit:" + str(unit) + ";method:" + str(method) + ";]"
+        myPrint("Not found in config [protocol:" + str(protocol) + ";model:" + str(model) + ";house:" + str(house) + ";unit:" + str(unit) + ";method:" + str(method) + ";]", "INFO")
             
     elif msensor is not None:
       protocol = msensor.group(1)
@@ -128,13 +161,29 @@ def parseReading(msg):
     
 with open(configpath + "config.yaml", 'r') as stream:
   out = yaml.safe_load(stream)
-  print "[info] getting the mqtt settings"
+  debug = ""
+  try:
+    debug = out['debug']
+  except:
+    debug = "false"
+  
+  if str(debug) == "true":
+    my_logger.setLevel(logging.DEBUG)
+    handler.setLevel(logging.DEBUG)
+  else:
+    my_logger.setLevel(logging.INFO)
+    handler.setLevel(logging.INFO)
+  
+  my_logger.addHandler(handler)
+  
+    
+  myPrint("getting the mqtt settings", "INFO")
   mqtt_host = out['mqtt']['host']
   mqtt_username = out['mqtt']['authentication']['username']
   mqtt_password = out['mqtt']['authentication']['password']
   mqtt_port = out['mqtt']['port']
 
-  print "[info] getting my sensors"
+  myPrint("getting my sensors", "INFO")
   sensors = []
   try:
     Sensor = namedtuple("Sensor", "id protocol model mqttRoom")
@@ -142,23 +191,23 @@ with open(configpath + "config.yaml", 'r') as stream:
       mysensor = Sensor(s['device']['id'],s['device']['protocol'], s['device']['model'], s['mqttRoom'])
       sensors.append(mysensor)
   except:
-    print "[info] No senors in config"
+    myPrint("No sensors in config", "INFO")
 
-  print "[info] getting my switches"
+  myPrint("getting my switches", "INFO")
   listeners = []
   try:
-    device = namedtuple("device", "mqttRoom mqttDescription protocol model house unit id sendcount")
+    device = namedtuple("device", "mqttRoom mqttDescription protocol model house unit id sendcount type")
     for sw in out['switch']:
       try:
         sendcount = sw['sendcount']
       except:
         sendcount = 1
-      myDevice = device(sw['mqtt']['room'],sw['mqtt']['description'],sw['device']['protocol'],sw['device']['model'],sw['device']['house'],sw['device']['unit'], str(sw['mqtt']['room'] + '/' + sw['mqtt']['description']),sendcount)
+      myDevice = device(sw['mqtt']['room'],sw['mqtt']['description'],sw['device']['protocol'],sw['device']['model'],sw['device']['house'],sw['device']['unit'], str(sw['mqtt']['room'] + '/' + sw['mqtt']['description']),sendcount, sw['device']['type'])
       listeners.append(myDevice)
   except:
-    print "[info] No switches in config"
+    myPrint("No switches in config", "INFO")
 
-  print "[info] getting my triggers"
+  myPrint("getting my triggers", "INFO")
   try:
     trigger = namedtuple("trigger", "mqttRoom mqttDescription protocol model house unit id sendcount")
     for r in out['trigger']:
@@ -169,11 +218,11 @@ with open(configpath + "config.yaml", 'r') as stream:
       myTrigger = trigger(r['mqtt']['room'],r['mqtt']['description'],r['device']['protocol'],r['device']['model'],r['device']['house'],r['device']['unit'], str(sw['mqtt']['room'] + '/' + sw['mqtt']['description']),sendcount)
       listeners.append(myTrigger)
   except:
-    print "[info] No switches in config"
+    myPrint("No switches in config", "INFO")
 
 
 def action_sub_thread():
-    print "start of mqtt sub"
+    myPrint("start of mqtt sub", "INFO")
     client = mqtt.Client()
     client.on_connect = on_connect
     client.on_message = on_message
@@ -198,8 +247,8 @@ def mqtt_trigger_handler(room,description,msg):
           lastState = 0
           
         if elapsed > datetime.timedelta(seconds=5) or int(lastState) != int(msg.payload):
-          print "MQTT device " + str(sw.id) + " setting state " + str(msg.payload)
-          q.put( (sw.protocol, sw.model, sw.house, sw.unit, msg.payload,sw.sendcount) )
+          myPrint("MQTT device " + str(sw.id) + " setting state " + str(msg.payload), "INFO")
+          q.put( (sw.protocol, sw.model, sw.house, sw.unit, msg.payload,sw.sendcount, sw.type) )
           with lock:
             shared_dict[sw.id] = (datetime.datetime.now(), msg.payload)
         return True
@@ -211,21 +260,22 @@ def on_message(client, userdata, msg):
     
     if m:
         if mqtt_trigger_handler(m.group(1),m.group(2),msg) == False:
-          print "Not listed trigger in room: [" + m.group(1) + "] with  description: [" + m.group(2)+ "]"
+          myPrint("Not listed trigger in room: [" + m.group(1) + "] with  description: [" + m.group(2)+ "]", "INFO")
     else:
-      print "Recived a topic that wasn't supported topic: ["+ msg.topic + "]" 
+      myPrint("Recived a topic that wasn't supported topic: ["+ msg.topic + "]", "INFO")
 
 def my_publish(topic, message):
     publish.single(topic, payload=message, qos=0, retain=False, hostname=mqtt_host,
     port=mqtt_port, client_id="", keepalive=60, will=None, auth= {'username':mqtt_username, 'password':mqtt_password}, tls=None,
     protocol=mqtt.MQTTv31)
 
-print "start thread 1"
+
+myPrint("start thread 1", "INFO")
 t = threading.Thread(target=action_sub_thread, args = ())
 t.daemon = True
 t.start()
 
-print "start thread 2"
+myPrint("start thread 2", "INFO")
 t2 = threading.Thread(target=reader, args = ())
 t2.daemon = True
 t2.start()
@@ -237,5 +287,3 @@ t3.start()
 while 1:
   time.sleep(0.1)
   pass
-
-
