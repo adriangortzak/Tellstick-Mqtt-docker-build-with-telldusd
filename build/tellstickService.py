32a2a169a81f91f896e7e98f2c379edd82585252
Adrian Gortzak <addegor95@gmail.com>
2017-05-27 11:50:02 +0200

    convert to json and then string (same bug with /')

--- build/tellstickService.py
+++ build/tellstickService.py
@@ -288,7 +288,7 @@ def senML(id,sensorValue, sensortype):
     data['v'] = sensorValue
     payload.append(data)
 
-    return (str(payload)).replace('\\","")
+    return (str(payload)).replace("\\","")
 
 myPrint("start thread 1", "INFO")
 t = threading.Thread(target=action_sub_thread, args = ())
